acafe69e8164f7489291eeb52a3f79d44716216d
dinesh07sengar <sengardante@gmail.com>
2023-04-20 00:03:41 +0530

    commit-3

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import App from './App'
 import './index.css'
 import{ChakraProvider} from "@chakra-ui/react"
 import{BrowserRouter} from "react-router-dom"
+import{Provider} from "react-redux"
+import { store } from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <ChakraProvider>
+    <Provider store={store}>
     <App />
+    </Provider>
   </ChakraProvider>
   </BrowserRouter>
   
