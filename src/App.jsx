90ac35e1bf1ac6bc6e6f480d1aa31605d06ef996
Pradeep <tripathipkr@gmail.com>
2023-04-21 11:19:28 +0530

    payment page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Payment from './components/Payment/Payment'
 
 function App() {
 
   return (
     <div className="App">
        Disney + HotStar......
+       
     </div>
   )
 }
---
+++ src/components/Payment/Payment.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Payment() {
+  return (
+    <div>
+      <h1>Payment page</h1>
+    </div>
+  )
+}
+
+export default Payment
